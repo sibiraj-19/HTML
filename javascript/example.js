689a589da8be6bbec546777af9c29a71317f8298
sibi <sibi7257@gmail.com>
2023-07-13 12:43:50 +0530

    commit

---
+++ javascript/example.js
@@ -0,0 +1,26 @@
+function myfun(){
+    document.getElementById('demo').innerHTML='hello!';
+
+document.getElementById('demo').style.color='green';
+document.getElementById('demo').style.display='block';
+alert("this file can't support by your system.");
+alert("your system is hacked");
+console.log(5+7);
+}
+function fun(){
+     let x,y,z;
+     x=3;
+     y=7;
+     z=10;
+     console.log(x+y+z);
+     console.log((x+y+z)/3);
+
+}
+
+// for output{
+//   innerHTML;
+//   console.log();
+//   window.alert();
+//  document.write();
+
+// }
